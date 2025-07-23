92ec73df0e5e7f48689b0c1e79f0c10d5e80c3e6
João Marcos Coutinho Nunes <jmcoutinhonunes@gmail.com>
2025-07-22 22:34:59 -0300

    feat: change port from 8000 to 8008

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8001))
-    uvicorn.run("api.api:app", host="0.0.0.0", port=8002, reload=True)
+    port = int(os.getenv("PORT", 8008))
+    uvicorn.run("api.api:app", host="0.0.0.0", port=8008, reload=True)
